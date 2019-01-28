3318224aa68e0eb8617a9153ccf949027d49a063
aprv04 <46750701+aprv04@users.noreply.github.com>
2019-01-28 17:16:22 +0530

    Workqueue

---
+++ .workqueue.ko.cmd
@@ -0,0 +1,1 @@
+cmd_/home/debian/DD/day5/workqueue/workqueue.ko := ld -EL -r  -T /usr/src/linux-headers-3.8.13-bone81/scripts/module-common.lds --build-id  -o /home/debian/DD/day5/workqueue/workqueue.ko /home/debian/DD/day5/workqueue/workqueue.o /home/debian/DD/day5/workqueue/workqueue.mod.o
--- Makefile
+++ Makefile
@@ -1,4 +1,4 @@
-obj-m=tasklet.o
+obj-m=workqueue.o
 KERN_DIR=/lib/modules/$(shell uname -r)/build
 PWD=$(shell pwd)
 
